0d3594919a99b9aee0ab41d4864134f8b91efd58
darkzyzxc <79263389+darkzyzxc@users.noreply.github.com>
2021-05-01 01:28:16 +0800

    Add files via upload

--- 語音.py
+++ 語音.py
@@ -15,9 +15,18 @@ async def on_ready():
 @bot.event
 async def on_voice_state_update(member,before,after):
   b = bot.get_channel(835343604209287228)
-  await b.send(f'{member}加入語音頻道!')
-   
+  if before.channel != None:#!= none 是否有加入語音頻道
+        guild = before.channel#抓取頻道名稱
+        print(f'{member}離開->{guild}語音頻道!')#print，記錄於本機伺服器檔案
+        await b.send(f'{member}離開->{guild}語音頻道!')#await .send，發送於群組
+  elif after.channel != None:#判斷是否離開，guild為頻道名稱變數
+        guild = after.channel
+        print(f'{member}加入->{guild}語音頻道!')
+        await b.send(f'{member}加入->{guild}語音頻道!')
+  
 
 
 
-bot.run('token')
+
+
+bot.run('NzM3NTcwNTk3OTk5MjE0Njcy.Xx_SJQ.jNpFb_4hrSkx24-BlhbjYJhh5OQ')
